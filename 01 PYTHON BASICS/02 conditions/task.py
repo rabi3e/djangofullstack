6c9ce0c107164bbc3dee64a3b153a84567207435
rabi3e <65637491+rabi3e@users.noreply.github.com>
2022-09-13 19:45:12 +0100

    task ok

---
+++ 01 PYTHON BASICS/02 conditions/task.py
@@ -0,0 +1,27 @@
+#Check if 10 is bigger than 15 or not
+print (10 > 15)
+
+#If 10 is not bigger than15 print x is smaller than 15
+
+if (10 > 15) == False :
+    print("x is smaller than 15")
+    
+#In which cases we will use all
+
+#What is the differences between all , and
+# What is the differences between any , or
+# If we need all the conditions to be true we will use ….
+# What is the differences between if , elif
+# What is the differences between elif else
+# Can we use more than one elif  ----> yes
+
+# Can we use more than one else  --- >  no
+
+# write s simple ternary operator
+# in elif , python will check all the conditions no matter what ?
+# in elif we use else for ... ?
+''' if we have this list [2,4,6,8,10] :
+        check to see if 4 in this list or not
+        check to see if 4 and 6 in this list on not
+        check to see if 3 or 6 in this list
+        check to see if 2 , 4 and 5 in this list '''
