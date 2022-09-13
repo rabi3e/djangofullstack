6fe87ca6fa6d4b4ba44b2ef6b73ce9a9487221b7
rabi3e <65637491+rabi3e@users.noreply.github.com>
2022-09-13 20:03:11 +0100

    task var and cond ok

--- 01 PYTHON BASICS/02 conditions/task.py
+++ 01 PYTHON BASICS/02 conditions/task.py
@@ -8,20 +8,40 @@ if (10 > 15) == False :
     
 #In which cases we will use all
 
+  
+
 #What is the differences between all , and
 # What is the differences between any , or
-# If we need all the conditions to be true we will use ….
+# If we need all the conditions to be true we will use ….  and
+
 # What is the differences between if , elif
+
 # What is the differences between elif else
+
 # Can we use more than one elif  ----> yes
 
 # Can we use more than one else  --- >  no
 
 # write s simple ternary operator
-# in elif , python will check all the conditions no matter what ?
+x=4
+print("x>5") if x>5 else  print ("x<5")
+# in elif , python will check all the conditions no matter what ? until found the corret one
+
 # in elif we use else for ... ?
+
 ''' if we have this list [2,4,6,8,10] :
         check to see if 4 in this list or not
         check to see if 4 and 6 in this list on not
         check to see if 3 or 6 in this list
         check to see if 2 , 4 and 5 in this list '''
+
+li = [1,2,3,4,5]
+if 4 in li :
+    print ("4 ")
+elif 4 and 6 in li :
+    print("4 and 6")
+elif 3 or 6 in li :
+    print("3 or6")
+elif 2 and 4 and 5 in li :
+    print("2 and 4 and 5")
+    
