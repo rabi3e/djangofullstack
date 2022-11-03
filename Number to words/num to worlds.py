70ffc4e3d39a960f67496bee4c12170b311166fc
RABIE CHATOUI <chatouirabie@gmail.com>
2022-11-03 23:17:48 +0100

    Delete Number to words directory

--- Number to words/num to worlds.py
+++
@@ -1,8 +0,0 @@
-#1 install num2world library
-from num2words import num2words
-x = int(input(" plz enter a numnber to convert ----> :"))
-#select language
-lang=  input("plz select uour language : fr, en, it, ar :------>")
-print(num2words(x,lang=lang))
-
-
